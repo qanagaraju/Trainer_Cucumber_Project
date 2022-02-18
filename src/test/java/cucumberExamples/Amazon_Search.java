37b70deec7273b9d6db75569a28732b67896c5ab
qanagaraju <nagarazu@gmail.com>
2022-02-18 08:39:02 +0530

    Second[F commit

--- src/test/java/cucumberExamples/Amazon_Search.java
+++ src/test/java/cucumberExamples/Amazon_Search.java
@@ -1,30 +1,38 @@
 package cucumberExamples;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import cucumber_App_Utilities.Browser_Factory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Amazon_Search {
 	
-	
+	static WebDriver driver;
+	static Browser_Factory browser = new Browser_Factory();
 	
 	@Given("user start amazon home page")
 	public void user_start_amazon_home_page() {
 	 
-		System.out.println("Start amazon home page");
+		driver = browser.startBrowser("chrome", "http://www.amazon.in");
+		
 	}
 	
 	
 	@When("User insert search details")
 	public void user_insert_search_details() {
-	    System.out.println("Insert amazon search data");
+	   
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("samsung mobiles");
+		
 	}
 	
 	
 	@Then("User click search button")
 	public void user_click_search_button() {
 	   
-		System.out.println("Click amazon button");
+		driver.findElement(By.id("nav-search-submit-button")).click();
 		
 	}
 	
@@ -32,7 +40,7 @@ public class Amazon_Search {
 	@Then("User close browser")
 	public void user_close_browser() {
 	   
-		System.out.println("close test");
+		driver.close();
 	}
 
 	
--- src/test/java/cucumberExamples/Cucumber_TestRunner_Class.java
+++ src/test/java/cucumberExamples/Cucumber_TestRunner_Class.java
@@ -9,9 +9,9 @@ import io.cucumber.testng.CucumberOptions;
 		
 		//How to run multiple feature files
 		
-		features = {"E:\\SeleniumFrame_Works\\Cucumber_Framework\\CucumberTest\\Cucucmber_Files\\Login.feature"		
-		},
-		glue = "cucumberExamples",
+		features = {"E:\\SeleniumFrame_Works\\Cucumber_Framework\\CucumberTest\\Cucucmber_Files\\Login.feature",	
+				   "E:\\SeleniumFrame_Works\\Cucumber_Framework\\CucumberTest\\Cucucmber_Files\\Amazon_Search.feature"},
+					glue = "cucumberExamples",
 				//plugin = { "pretty", "html:target/cucumber-reports" },
 				//plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json"},
 				plugin = { "pretty", "junit:target/cucumber-reports/Cucumber.xml","junit:target/cucumber-reports/Cucumber.xml"},
